4a2de29f71896af1e777c16152d4886f38e5a37d
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-10-10 18:00:46 -0300

    feat(checkout): uses input component

--- src/pages/Checkout/CheckoutForm/CheckoutForm.tsx
+++ src/pages/Checkout/CheckoutForm/CheckoutForm.tsx
@@ -1,15 +1,14 @@
 import { useFormContext } from "react-hook-form";
 import VMasker from "vanilla-masker";
-import {
-  Field,
-  GridContainer,
-  Input,
-  OptionalPlaceholder,
-} from "./CheckoutForm.styles";
+
+import { Input } from "src/components/Input";
+
+import { GridContainer } from "./CheckoutForm.styles";
 
 export function CheckoutForm() {
-  const { register, watch } = useFormContext();
+  const { register, watch, formState } = useFormContext();
 
+  const { errors } = formState;
   const postalCodeValue = watch("postalCode", "");
 
   return (
@@ -18,27 +17,50 @@ export function CheckoutForm() {
         <Input
           placeholder="CEP"
           value={VMasker.toPattern(postalCodeValue, "99999-999")}
+          error={errors.postalCode?.message?.toString()}
           {...register("postalCode")}
         />
       </GridContainer>
       <GridContainer templateColumns="1fr">
-        <Input placeholder="Rua" {...register("street")} />
+        <Input
+          placeholder="Rua"
+          error={errors.street?.message?.toString()}
+          {...register("street")}
+        />
       </GridContainer>
       <GridContainer templateColumns="1fr 2fr">
-        <Input type="number" placeholder="Número" {...register("number")} />
-        <Field>
-          <Input
-            placeholder="Complemento"
-            className="addressDetails"
-            {...register("details")}
-          />
-          <OptionalPlaceholder>Opcional</OptionalPlaceholder>
-        </Field>
+        <Input
+          type="number"
+          placeholder="Número"
+          error={errors.number?.message?.toString()}
+          {...register("number", {
+            valueAsNumber: true,
+          })}
+        />
+        <Input
+          placeholder="Complemento"
+          required={false}
+          error={errors.details?.message?.toString()}
+          {...register("details")}
+        />
       </GridContainer>
-      <GridContainer templateColumns="1fr 2fr 6rem">
-        <Input placeholder="Bairro" {...register("address")} />
-        <Input placeholder="Cidade" {...register("city")} />
-        <Input placeholder="UF" maxLength={2} {...register("state")} />
+      <GridContainer templateColumns="2fr 2fr 1fr">
+        <Input
+          placeholder="Bairro"
+          error={errors.address?.message?.toString()}
+          {...register("address")}
+        />
+        <Input
+          placeholder="Cidade"
+          error={errors.city?.message?.toString()}
+          {...register("city")}
+        />
+        <Input
+          placeholder="UF"
+          error={errors.state?.message?.toString()}
+          maxLength={2}
+          {...register("state")}
+        />
       </GridContainer>
     </GridContainer>
   );
