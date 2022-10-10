0b643e4dc2bfd8127cf59dea81564af1c9189532
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-10-10 14:36:27 -0300

    feat(header): add link to checkout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { Link } from "react-router-dom";
 
 import { Assets } from "src/assets";
 import { Button } from "src/components/Button";
@@ -17,10 +18,12 @@ export function Header() {
           <MapPin size={20} weight="fill" />
           Porto Alegre, RS
         </Button>
-        <Button variant="yellow-light">
-          {totalAmount > 0 && <TotalAmount>{totalAmount}</TotalAmount>}
-          <ShoppingCart size={20} weight="fill" />
-        </Button>
+        <Link to="/checkout">
+          <Button variant="yellow-light">
+            {totalAmount > 0 && <TotalAmount>{totalAmount}</TotalAmount>}
+            <ShoppingCart size={20} weight="fill" />
+          </Button>
+        </Link>
       </Actions>
     </HeaderContainer>
   );
