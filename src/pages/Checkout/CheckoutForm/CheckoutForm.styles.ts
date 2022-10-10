f4dc7cec80434ce2721693acd2ff54508c6bc8ca
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-10-10 16:26:24 -0300

    feat(checkout): add checkout form

---
+++ src/pages/Checkout/CheckoutForm/CheckoutForm.styles.ts
@@ -0,0 +1,46 @@
+import styled, { css } from "styled-components";
+
+type GridContainerProps = {
+  templateColumns: string;
+};
+
+export const GridContainer = styled.div<GridContainerProps>`
+  display: grid;
+  grid-template-columns: ${({ templateColumns }) => templateColumns};
+  gap: 1.6rem;
+`;
+
+export const Field = styled.fieldset`
+  position: relative;
+  border: 0;
+`;
+
+export const OptionalPlaceholder = styled.span`
+  position: absolute;
+  color: ${({ theme }) => theme["base-label"]};
+  font-size: 1.2rem;
+  font-weight: 400;
+  font-style: italic;
+  right: 1.2rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+export const Input = styled.input`
+  font-size: 1.4rem;
+  border-radius: 0.4rem;
+
+  width: 100%;
+  padding: 1.2rem;
+
+  ${({ theme }) => css`
+    background: ${theme["base-input"]};
+    border: 0.1rem solid ${theme["base-button"]};
+
+    color: ${theme["base-text"]};
+
+    &::placeholder {
+      color: ${theme["base-label"]};
+    }
+  `}
+`;
---
+++ src/pages/Checkout/CheckoutForm/CheckoutForm.tsx
@@ -0,0 +1,41 @@
+import { useFormContext } from "react-hook-form";
+import VMasker from "vanilla-masker";
+import {
+  Field,
+  GridContainer,
+  Input,
+  OptionalPlaceholder,
+} from "./CheckoutForm.styles";
+
+export function CheckoutForm() {
+  const { register, watch } = useFormContext();
+
+  const postalCodeValue = watch("postalCode");
+
+  return (
+    <GridContainer templateColumns="1fr">
+      <GridContainer templateColumns="1fr 2fr">
+        <Input
+          placeholder="CEP"
+          value={VMasker.toPattern(postalCodeValue, "99999-999")}
+          {...register("postalCode")}
+        />
+      </GridContainer>
+      <GridContainer templateColumns="1fr">
+        <Input placeholder="Rua" {...register("street")} />
+      </GridContainer>
+      <GridContainer templateColumns="1fr 2fr">
+        <Input type="number" placeholder="Número" {...register("number")} />
+        <Field>
+          <Input placeholder="Complemento" {...register("details")} />
+          <OptionalPlaceholder>Opcional</OptionalPlaceholder>
+        </Field>
+      </GridContainer>
+      <GridContainer templateColumns="1fr 2fr 6rem">
+        <Input placeholder="Bairro" {...register("address")} />
+        <Input placeholder="Cidade" {...register("city")} />
+        <Input placeholder="UF" maxLength={2} {...register("state")} />
+      </GridContainer>
+    </GridContainer>
+  );
+}
