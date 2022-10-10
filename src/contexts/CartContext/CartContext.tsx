ba0db27aade262bf7fcee405b51c1a2451a10c9d
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-10-10 13:15:14 -0300

    feat(contexts): add cart context

---
+++ src/contexts/CartContext/CartContext.tsx
@@ -0,0 +1,125 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+} from "react";
+import { v4 as uuid } from "uuid";
+
+import { Coffee } from "src/lib/Coffees";
+
+import {
+  addNewCartItemAction,
+  CartItem,
+  updateAmountCartItemAction,
+} from "src/reducers/cart/actions";
+import { cartReducer } from "src/reducers/cart/reducer";
+
+type CartProviderProps = {
+  children: ReactNode;
+};
+
+type CartContextData = {
+  items: CartItem[];
+  totalAmount: number;
+  getCartItem: (coffee: Coffee) => CartItem | undefined;
+  incrementAmount: (coffee: Coffee) => void;
+  decrementAmount: (coffee: Coffee) => void;
+  createNewCartItem: (coffee: Coffee, amount: number) => void;
+};
+
+const CartContext = createContext({} as CartContextData);
+
+export function CartProvider({ children }: CartProviderProps) {
+  const [cartState, dispatch] = useReducer(
+    cartReducer,
+    {
+      items: [],
+    },
+    (state) => {
+      const storedStateAsJSON = localStorage.getItem(
+        "@coffee-delivery:cart-state-1.0.0"
+      );
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+
+      return state;
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem(
+      "@coffee-delivery:cart-state-1.0.0",
+      JSON.stringify(cartState)
+    );
+  }, [cartState]);
+
+  function createNewCartItem(coffee: Coffee, amount: number) {
+    const cartItem: CartItem = {
+      id: uuid(),
+      amount,
+      product: coffee,
+    };
+
+    dispatch(addNewCartItemAction(cartItem));
+  }
+
+  function getCartItem(coffee: Coffee) {
+    return cartState.items.find(
+      (cartItem) => cartItem.product.name === coffee.name
+    );
+  }
+
+  function incrementAmount(coffee: Coffee) {
+    const cartItem = getCartItem(coffee) ?? {
+      amount: 1,
+      id: uuid(),
+      product: coffee,
+    };
+
+    dispatch(updateAmountCartItemAction(cartItem, cartItem.amount + 1));
+  }
+
+  function decrementAmount(coffee: Coffee) {
+    const cartItem = getCartItem(coffee);
+
+    if (!cartItem) return;
+
+    const newAmount = cartItem.amount - 1;
+
+    if (newAmount < 0) return;
+
+    dispatch(updateAmountCartItemAction(cartItem, newAmount));
+  }
+
+  const totalAmount = useMemo(() => {
+    return cartState.items.reduce((total, { amount }) => {
+      return total + amount;
+    }, 0);
+  }, [cartState]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        items: cartState.items,
+        totalAmount,
+        getCartItem,
+        createNewCartItem,
+        incrementAmount,
+        decrementAmount,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCartProvider() {
+  const context = useContext(CartContext);
+
+  return context;
+}
